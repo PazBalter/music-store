bc24e0e749d50cb288d7bce5f37918e1a53a26f4
Paz Balter <pazbalterpb@gmail.com>
2022-05-29 23:47:16 +0300

    navheroandicon

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -1,20 +1,27 @@
 <template>
   <header 
-   :class="{ 'scrolled-nav' : scrollPosition }">
+   :class="{ 'scrolled-nav' : scrolledNav }">
       <nav>
           <div class="header-logo">
-              <img src="@/assets/logo.png" alt="">
+            <router-link :to="{ name: 'Home' }"><img src="@/assets/logo.png" alt=""></router-link>
           </div>
           <ul v-show="!mobile" class="navigation">
               <li><router-link class="link" :to="{ name: 'Shop' }">Shop</router-link></li>
               <!-- <li><router-link class="link" :to="{ name: '' }"></router-link></li> -->
-              <li><router-link class="link" :to="{ name: 'About' }">Home</router-link></li>
+              <li><router-link class="link" :to="{ name: 'About' }">About us</router-link></li>
               <li><router-link class="link" :to="{ name: 'Contact' }">Contact</router-link></li>
           </ul>
           <div class="hamburger-icon">
-              <i @click="toggleMobileNav" v-show="mobile" :class="{'icon-active': mobileNav}"></i>
+              <i @click="toggleMobileNav" v-show="mobile" class="far fa-bars material-symbols-outlined" :class="{'icon-active': mobileNav}">menu</i>
           </div>
-          <transition name="mobile-nav"></transition>
+          <transition name="mobile-nav">
+            <ul v-show="mobileNav" class="dropdown-nav">
+              <li><router-link class="link" :to="{ name: 'Shop' }">Shop</router-link></li>
+              <!-- <li><router-link class="link" :to="{ name: '' }"></router-link></li> -->
+              <li><router-link class="link" :to="{ name: 'About' }">About us</router-link></li>
+              <li><router-link class="link" :to="{ name: 'Contact' }">Contact</router-link></li>
+            </ul>
+          </transition>
           
       </nav>
   </header>
@@ -24,10 +31,178 @@
  export default {
  data() {
     return {
-      scrollPosition: true,
-      mobile: false,
-     
+      scrolledNav: null,
+      mobile:null,
+      mobileNav:null,
+      windowWidth: null,
     };
   },
- }
+  created() {
+    window.addEventListener("resize", this.checkScreen)
+    this.checkScreen()
+  },
+  mounted(){
+    window.addEventListener("scroll", this.updateScroll)
+
+  },
+  methods:{
+    toggleMobileNav(){
+      this.mobileNav = !this.mobileNav
+    },
+
+    updateScroll(){
+      const scrollPosition = window.scrollY
+      if(scrollPosition > 50){
+        this.scrolledNav = true;
+        return;
+      }
+      this.scrolledNav = false;
+    },
+
+    checkScreen(){
+      this.windowWidth = window.innerWidth
+      if(this.windowWidth <= 750){
+        this.mobile = true
+        return
+      }
+      this.mobile= false
+      this.mobileNav = false
+      return
+    }
+  }
+ };
  </script>
+
+<style lang="scss" scoped>
+header{
+  background-color: rgba(0,0,0,0.8);
+  z-index: 99;
+  width: 100%;
+  position: fixed;
+  transition: 0.5s ease all;
+  color: #fff;
+
+  nav{
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    padding: 12px 0;
+    transition: .5s ease all;
+    width: 90%;
+    margin: 0 auto;
+    @media (min-width: 1140px){
+      max-width: 1140px;
+    }
+
+    ul,
+    .link{
+      font-weight: 500;
+      color: #fff;
+      list-style: none;
+      text-decoration: none;
+    }
+
+    li{
+      text-transform: uppercase;
+      padding: 16px;
+      margin-left: 16px;
+    }
+
+    .link {
+      font-size: 14px;
+      transition: .5s ease all;
+      padding-bottom: 4px;
+      border-bottom: 1px solid transparent
+    }
+    .link:hover {
+      color: red;
+      border-color: red;
+    }
+
+    .header-logo{
+      display: flex;
+      align-items: center;
+
+      img{
+        width:110px;
+        transition: .5s ease all;
+      }
+    }
+
+    .navigation{
+      display: flex;
+      align-items: center;
+      flex: 1;
+      justify-content: flex-end;
+    }
+
+    .hamburger-icon{
+      display: flex;
+      align-items: center;
+      position: absolute;
+      top: 0;
+      right: 24px;
+      height: 100%;
+      
+      i{
+        cursor: pointer;
+        font-size: 24px;
+        transition: .8s ease all;
+      }
+    }
+
+
+    .icon-active{
+      transform: rotate(180deg);
+    }
+    .dropdown-nav{
+      display: flex;
+      flex-direction: column;
+      position: fixed;
+      width:100%;
+      max-width: 250px;
+      height: 100%;
+      background-color: #fff;
+      top: 0;
+      left: 0;
+
+      li{
+        margin-left: 0;
+        .link{
+          color:#000
+        }
+      }
+    }
+    .mobile-nav-enter-active,
+    .mobile-nav-leave-active{
+      transition: 1s ease all;
+    }
+
+    .mobile-nav-enter-from,
+    .mobile-nav-leave-to{
+      transform: translateX(-250px);
+    }
+
+    .mobile-nav-enter-to{
+      transform: translateX(0);
+    }
+  
+  }
+}
+
+.scrolled-nav{
+  background-color: #000;
+  box-shadow: 0 4x 6px -1px rgba(0,0,0,0.1), 0 4x 6px -1px rgba(0,0,0,0.06);
+
+  nav {
+    padding: 8px 0;
+    
+    .header-logo{
+      img{
+        width: 100px;
+        box-shadow: 0 4x 6px -1px rgba(0,0,0,0.1), 0 4x 6px -1px rgba(0,0,0,0.06);
+      }
+    }
+  }
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="home">Home
+  <div class="home">
+    <section class="hero">
+      <div class="container">
+      
+      </div>
+    </section>
   </div>
 </template>
 
@@ -12,3 +17,22 @@ export default {
 
 }
 </script>
+
+<style lang="scss" scoped>
+.hero{
+  width: 100%;
+  background-image:url("../assets/store-hero.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+  background-size:cover;
+  height: 120vh;
+
+  img{
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+  }
+
+}
+</style>
